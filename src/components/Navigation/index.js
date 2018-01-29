855319e62862fb88ae4628c653dfc5da995df378
kkamburov <Kaloyan.Kamburov@experian.com>
2018-01-29 17:54:53 +0200

    in progress

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navigation = (props) => {
+    return(
+        <div>{props.navigation_title}</div>
+    )
+}
+
+export default Navigation;
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,7 +1,16 @@
 import React, {  Component } from 'react'
 import LoginForm from '../../components/LoginForm'
+import Navigation from '../../components/Navigation';
 
 import { connect } from 'react-redux'
+
+// class Login extends Component {
+//     render() {
+//         return (
+//             <LoginForm name={this.props.name}/>
+//         )
+//     }
+// }
 
 const mapStateToProps = state => {
     return {
@@ -10,3 +19,4 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(LoginForm)
+// export default connect(mapStateToProps)(Navigation)
